c1f832043ec985b34e3aeff2682f0c437cab75b6
Emmanuel <eecheconea@trabajo.gob.ar>
2022-02-18 18:05:45 -0300

    actualizo controlador pedidos
    agrego endpoint a el controlador de pedidos

--- src/main/java/com/tiendapeliculas/tiendaPeliculas/controller/pedidos/PedidosController.java
+++ src/main/java/com/tiendapeliculas/tiendaPeliculas/controller/pedidos/PedidosController.java
@@ -1,11 +1,44 @@
 package com.tiendapeliculas.tiendaPeliculas.controller.pedidos;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tiendapeliculas.tiendaPeliculas.model.Pedido;
 
 @RestController
 @RequestMapping("/pedido")
 public class PedidosController {
 	
+	@GetMapping("/obtener")
+	public List<Pedido> obtenerPedidos()
+	{
+		return null;
+	}
 	
+	@GetMapping("/obtener/{id}")
+	public Pedido obtenerPedido(@RequestParam int id)
+	{
+		return null;
+	}
+	
+	@PostMapping("/insertar")
+	public ResponseEntity<String> insertarPedido(@RequestBody Pedido pedido)
+	{
+		
+		return null;
+	}
+	
+	@PutMapping("/actualizarEstado/{id}")
+	public ResponseEntity<String> actualizarEstado(@RequestParam int id)
+	{
+		return null;
+	}
 }
